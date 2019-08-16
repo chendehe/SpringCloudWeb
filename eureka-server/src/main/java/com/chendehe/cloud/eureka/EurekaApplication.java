4862a48b5274c8549e6fdd7d87de55440d7959be
chendehe <chendehe22@163.com>
2019-08-16 11:02:48 +0800

    eureka 配置

--- eureka-server/src/main/java/com/chendehe/cloud/eureka/EurekaApplication.java
+++ eureka-server/src/main/java/com/chendehe/cloud/eureka/EurekaApplication.java
@@ -3,6 +3,9 @@ package com.chendehe.cloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -12,4 +15,13 @@ public class EurekaApplication {
     SpringApplication.run(EurekaApplication.class, args);
   }
 
+  @EnableWebSecurity
+  class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.csrf().ignoringAntMatchers("/eureka/**");
+      super.configure(http);
+    }
+  }
 }
